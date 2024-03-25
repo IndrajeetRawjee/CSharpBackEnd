431bb5187b279ef6c99bf450410dd47600ce390f
Indrajeet Rawjee <indrajeetrawjee4@gmail.com>
2024-03-25 16:24:42 +0530

    Dynamics Complete. Exception Handling Started

---
+++ DynamicTrain/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DynamicTrain
+{
+    //Type Resolutions:
+    //static languages: Compile Time ()
+    //dynamic languages: Run Time
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
