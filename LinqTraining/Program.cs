c705cfc0f1fb9db1bd2c8a701186e56677aec0a6
Indrajeet Rawjee <indrajeetrawjee4@gmail.com>
2024-03-25 14:51:21 +0530

    LinQ Complete. Starting Nullable Types

--- LinqTraining/Program.cs
+++ LinqTraining/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Linq;
 
 namespace LinqTraining
 {
+    // What is LINQ?
+    //Stands for Language Integrated Query
+    // Gives capability to Query Objects.
+
+    // What all can we Query?
+    //Objects in Memory, E.g. Collections (LINQ to Object)
+    //Databases (LINQ to Entities)
+    //XML (LINQ to XML)
+    //ADO.NET Dat seta (LinQ to DataSets)
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var books = new BookRepository().GetBooks();
+
+            //LinQ Query ( Less Powerful than LinQ Extension Method [Query is converted into LinQ Extension Method] )
+            var cheaperBooks =
+                from b in books
+                where b.price < 10
+                orderby b.Title
+                select b;
+
+
+            //LinQ Extension Method ( More Powerful than LinQ Query )
+            var cheapBooks = books
+                                .Where(b => b.price < 10)
+                                .OrderBy(b => b.Title)
+                                .Select(b => b.Title);
+              
+            foreach(var book in cheapBooks)
+            {
+                Console.WriteLine(book);
+            }
         }
     }
 }
