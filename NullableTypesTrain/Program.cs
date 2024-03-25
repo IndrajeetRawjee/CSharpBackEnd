fa97fccd25421722f33ea19445238ffacb8f9858
Indrajeet Rawjee <indrajeetrawjee4@gmail.com>
2024-03-25 15:09:52 +0530

    Compkleted Nullable Types and Starting Dynamics.

--- NullableTypesTrain/Program.cs
+++ NullableTypesTrain/Program.cs
@@ -2,6 +2,9 @@
 
 namespace NullableTypesTrain
 {
+    //What are Nullable types.
+    //Values cannot  be null. But there might arise a case where you need value as null. Here nullable types are used
+    // ?? Null Qualicy operator --- Var = Value ?? DefaultValueOnNull
     class Program
     {
         static void Main(string[] args)
